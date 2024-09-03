8129aa5ff8488733821c8b15dd85d681ece4c1fb
Alfred76-web <alfred.njaku@strathmore.edu>
2024-09-03 12:55:11 +0300

    #variables

---
+++ variables.js
@@ -0,0 +1,23 @@
+// Declare variables
+let name = "Victoria Mwendwa";
+let age = 15;
+let isStudent = false;
+
+// Log variables to the console
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+// Convert string to number and add 10
+let numberString = "42";
+let number = parseInt(numberString);
+let result = number + 10;
+console.log("Result:", result);
+
+// Declare null and undefined variables
+let nullValue = null;
+let undefinedValue = undefined;
+
+// Log null and undefined to the console
+console.log("Null Value:", nullValue);
+console.log("Undefined Value:", undefinedValue);
